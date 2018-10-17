3b1594082750fb1cb58cc12f1dc7c2397466bb94
sudo-cho <noisiv@outlook.fr>
2018-10-17 09:36:02 +0200

    first commit godot

---
+++ KinematicBody2D.gd
@@ -0,0 +1,16 @@
+extends KinematicBody2D
+
+export (int) var speed = 200
+
+var target = Vector2()
+var velocity = Vector2()
+
+func _input(event):
+    if event.is_action_pressed('click'):
+        target = get_global_mouse_position()
+
+func _physics_process(delta):
+    velocity = (target - position).normalized() * speed
+    # rotation = velocity.angle()
+    if (target - position).length() > 5:
+        move_and_slide(velocity)
